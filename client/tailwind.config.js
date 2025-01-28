45c88875b4437a494dcef10efb3deacef0090e38
luuquangtri95 <luuquangtri95@gmail.com>
2025-01-28 21:36:19 +0700

    update code order with user_id

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,9 @@
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+		"./node_modules/react-tailwindcss-datepicker/dist/index.esm.{js,ts}",
+	],
 	theme: {
 		extend: {},
 	},
