9d5d76982d8e075dbc64a4073394bc441a41101d
luuquangtri95 <luuquangtri95@gmail.com>
2025-02-14 02:41:36 +0700

    remove log vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,19 +2,12 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig(({ mode }) => {
-	// Đường dẫn thư mục gốc (root)
-	// const rootPath = path.resolve(__dirname, ".."); // Đi lên 1 cấp để tới thư mục gốc
-	// const env = loadEnv(mode, rootPath); // Load các biến môi trường từ root
-
 	return {
 		plugins: [react()],
-		define: {
-			// "process.env.VITE_API_URL": JSON.stringify(env.VITE_API_URL),
-		},
+		define: {},
 		envDir: "../",
 		server: {
 			host: true, // Cần thiết để Vite hoạt động trong Docker
-			// port: parseInt(env.VITE_PORT) || 5173,
 			watch: {
 				usePolling: true, // Cần thiết cho hot reload trong Docker
 			},
